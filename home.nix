a504524dad79ec8cc3e870550e50daff1b6aeec6
kyaimffxx7 <mask2live@gmail.com>
2025-05-14 15:37:59 +0800

    config for nixos 2025-5-14

---
+++ home.nix
@@ -0,0 +1,118 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "hisoka";
+  home.homeDirectory = "/home/hisoka";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "23.05"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = with pkgs;[
+    # # Adds the 'hello' command to your environment. It prints a friendly
+    # # "Hello, world!" when run.
+    # pkgs.hello
+
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+
+    # Virural Tools
+    devbox
+    distrobox
+
+    # Note & Read
+    anytype
+    calibre
+
+    # IDE
+    jetbrains.pycharm-community
+    
+    # Broswer
+    brave
+    tor-browser
+
+    # Video & Picture Editor
+    gimp
+    krita
+
+
+  ];
+
+  ###########################
+  # Programs Configurations #
+  ###########################
+  # Git
+  programs.git = {
+    enable = true;
+    userName  = "kyaimffxx7";
+    userEmail = "mask2live@gmail.com";
+    lfs.enable = true;
+  };
+
+  ## waybar for hyprland wm
+  # programs.waybar = {
+  #   enable = true;
+  #   style = builtins.readFile ./waybar/style.css;
+  #   systemd.enable = true;
+  # };
+
+  # Setup emacs
+  # services.emacs.enable = true;
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # You can also manage environment variables but you will have to manually
+  # source
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/hisoka/etc/profile.d/hm-session-vars.sh
+  #
+  # if you don't want to manage your shell through Home Manager.
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # Emacs custom ## doom-emacs is BROKEN
+  # programs.doom-emacs = {
+  #  enable = true;
+  #  emacsPackage = pkgs.emacs-gtk;
+  # };
+}
